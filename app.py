786dbee79a9f60370a43b97b40a0c42aec58734a
Arpit Agrawal <agrawal.arpit03@nmims.edu.in>
2025-06-09 18:46:42 +0200

    added a form into the app.py file

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 
 app = Flask(__name__)
 
@@ -9,6 +9,22 @@ def welcome():
 @app.route('/index', methods=['GET'])
 def index():
     return "welcome to the index page!"
+
+@app.route('/sucess/<int:score>', methods=['GET'])
+def sucess(score):
+    return "Congratulations! You scored " + str(score) + "!"
+
+@app.route('/form', methods=['GET','POST'])
+def form():
+    if request.method == 'GET':
+        return render_template('form.html')
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        message = request.form['message']
+        retrunmessage = "Thank you " + name + "! Your email is " + email + " and your message is " + message + "!"
+        return render_template('form.html', message=retrunmessage)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
